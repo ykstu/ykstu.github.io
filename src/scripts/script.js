e77797063c9a5882aa6c68fa1938badeb358a2e1
ykstu <161049550+ykstu@users.noreply.github.com>
2024-02-25 18:31:12 +0800

    0.1.1

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,11 +1,3 @@
-/*
- *                       _       __      _     
- *       _______________(_)___  / /_    (_)____
- *      / ___/ ___/ ___/ / __ \/ __/   / / ___/
- *     (__  ) /__/ /  / / /_/ / /__   / (__  ) 
- *    /____/\___/_/  /_/ .___/\__(_)_/ /____/  
- *                    /_/         /___/        
- */
 function change_theme(theme){
   document.getElementById('yk').setAttribute("color-scheme", theme);
   document.getElementById('yk').setAttribute("data-bs-theme", theme);
