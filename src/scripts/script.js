073dd907a459af2e44aede15afef6177a9cc006e
ykstu <161049550+ykstu@users.noreply.github.com>
2024-03-09 18:22:44 +0800

    0.2.0

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,6 +2,3 @@ function change_theme(theme){
   document.getElementById('yk').setAttribute("color-scheme", theme);
   document.getElementById('yk').setAttribute("data-bs-theme", theme);
 }
-
-
-//导航栏
